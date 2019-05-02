4d5d13f4878708283547ff5bb26762022271286b
Boleyn Su <boleyn.su@gmail.com>
2019-05-02 22:21:56 +0800

    update

--- cmd.py
+++ cmd.py
@@ -20,9 +20,9 @@ def sendTo(emails, message):
   emails = [ email for email in emails if email != "mailer-daemon@googlemail.com" ]
   if not emails:
     return
-  print(", ".join(emails))
-  print(message.split("\n")[0])
-  print(message)
+#  print(", ".join(emails))
+#  print(message.split("\n")[0])
+#  print(message)
   msg = MIMEText(message, _charset="UTF-8")
   msg["From"] = "Group Gathering <" + EMAIL + ">"
   msg["To"] = ", ".join(emails)
