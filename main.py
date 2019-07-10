bc657cd2c18527e9796c0a8ee5e0a24bd6a9e4b2
Boleyn Su <boleyn.su@gmail.com>
2019-07-11 01:32:48 +0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import os, sys, time, subprocess, smtplib, imapclient, email, pyzmail, ssl
 from datetime import datetime
 from email.mime.text import MIMEText
